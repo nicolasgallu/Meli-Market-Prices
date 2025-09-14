a52dc91a73981a7b722316a913cf379e4c618ed4
nicolasgallu <nicolasgallu@pm.me>
2025-09-14 19:20:40 -0300

    fixing manager.py

--- google_manager.py
+++ google_manager.py
@@ -1,20 +1,19 @@
+import google.auth
 from google.cloud import secretmanager
 import json
-import os
 
 def load_service_account():
-    # Cloud Functions sets GOOGLE_CLOUD_PROJECT automatically
-    project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
-    secret_id = "nicogallu-account-service"  # must match the secret name in Secret Manager
-    
+    # Get project ID dynamically
+    _, project_id = google.auth.default()
+
+    secret_id = "nicogallu-account-service"
     name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    
+
     client = secretmanager.SecretManagerServiceClient()
     response = client.access_secret_version(name=name)
-    
+
     payload = response.payload.data.decode("UTF-8")
-    
-    # Return JSON if possible, otherwise raw string
+
     try:
         return json.loads(payload)
     except json.JSONDecodeError:
