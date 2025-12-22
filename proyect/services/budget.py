e51442cf881973054695b210e641db6bd348783c
nicolasgallu <nicolasgallu@pm.me>
2025-12-22 15:14:28 -0300

    changing message to spn

--- proyect/services/budget.py
+++ proyect/services/budget.py
@@ -6,5 +6,5 @@ def remain_budget(api_key):
     credist_left = response.json().get("subscription").get("usage").get("scrape").get("remaining")
     subscription_started_at = response.json().get("subscription").get("period").get("start")
     subscription_ended_at = response.json().get("subscription").get("period").get("end")
-    data = f"Scrapping Finalizado\ncredits_left: {credist_left}\nsubscription_started_at: {subscription_started_at}\nsubscription_ended_at: {subscription_ended_at}"
+    data = f"Scrapping Finalizado\ncreditos restantes: {credist_left}\nfecha inicio de subscripcion: {subscription_started_at}\nfecha fin de subscripcion: {subscription_ended_at}"
     return data,credist_left
