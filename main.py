99b950cdddaa53db20707d6c39e9dbacd43c683d
nicolasgallu <nicolasgallu@pm.me>
2025-12-21 22:33:36 -0300

    adding communication to telegram notification service

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from proyect.services.budget import remain_budget
 from proyect.services.load_scrapp import post_results_to_sheet  
 from proyect.services.cleaning_jsons import remove_data
 from proyect.services.protection import SheetProtector
+from proyect.services.telegram_notify import send_message
 from dotenv import load_dotenv
 import os
 
@@ -16,9 +17,12 @@ owner_email = os.getenv("OWNER_EMAIL")
 sheet_name = os.getenv("SHEET_NAME")
 scopes = os.getenv("SCOPES").split(",")
 api_key = os.getenv("SCRAPFLY_API_KEY")
+telegram_chat_ids = os.getenv("TELEGRAM_CHAT_IDS").split(",")
+url_wh = os.getenv("URL_WH")
 
 def main():
     """Entrypoint for Google Cloud Functions"""
+    send_message(url_wh, "comenzando scrapping", telegram_chat_ids)
     service_account = load_service_account()
     get_urls_from_sheet(service_account, scopes, spreadsheet_id, sheet_name)
     protecion = SheetProtector(service_account, scopes, spreadsheet_id, sheet_name, owner_email)
@@ -26,8 +30,9 @@ def main():
     scrap_meli_urls(api_key)
     scrap_urls_failed(api_key)
     merge_scraping()
-    remain = remain_budget(api_key)
-    post_results_to_sheet(service_account, scopes, spreadsheet_id, remain, sheet_name)
+    budget_data,credist_left = remain_budget(api_key)
+    post_results_to_sheet(service_account, scopes, spreadsheet_id, credist_left, sheet_name)
     remove_data()
     protecion.unprotect()
+    send_message(url_wh, budget_data, telegram_chat_ids)
 main()
--- proyect/services/budget.py
+++ proyect/services/budget.py
@@ -3,5 +3,8 @@ import requests
 def remain_budget(api_key):
     url = f"https://api.scrapfly.io/account?key={api_key}"
     response = requests.get(url)
-    credits = response.json().get("subscription").get("usage").get("scrape").get("remaining")
-    return credits
+    credist_left = response.json().get("subscription").get("usage").get("scrape").get("remaining")
+    subscription_started_at = response.json().get("subscription").get("period").get("start")
+    subscription_ended_at = response.json().get("subscription").get("period").get("end")
+    data = f"Scrapping Finalizado\ncredits_left: {credist_left}\nsubscription_started_at: {subscription_started_at}\nsubscription_ended_at: {subscription_ended_at}"
+    return data,credist_left
